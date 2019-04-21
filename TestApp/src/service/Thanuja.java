1fbbf8d3b0074e7a75cf3b6bd82ad7c0d0ff1588
Thanuja Chamika <Thanuja Chamika@DESKTOP-OIIMTVH>
2019-04-21 09:18:07 +0530

    deleted name

--- TestApp/src/service/Thanuja.java
+++ TestApp/src/service/Thanuja.java
@@ -2,10 +2,6 @@ package service;
 
 public class Thanuja {
 
-  Thanuja
-  Chamika
-  Wijesinghe
-
   system.out.println("Thanuja");
   system.out.println("Chamika");
   system.out.println("Wijesinghe");
